1f1f05bf66d2b6c514a697dd6bad5c0ebfcaee7a
Tommaso <scquizzatotommaso@me.com>
2014-12-02 09:52:01 +0100

    Voci del menu
    Aggiunte le voci dei menu

--- app/src/main/java/com/scquizzatotommaso/misterfutsal/MainActivity.java
+++ app/src/main/java/com/scquizzatotommaso/misterfutsal/MainActivity.java
@@ -1,6 +1,7 @@
 package com.scquizzatotommaso.misterfutsal;
 
 import android.app.Activity;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -37,6 +38,8 @@ public class MainActivity extends ActionBarActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
         mTitle = getTitle();
@@ -62,11 +65,9 @@ public class MainActivity extends ActionBarActivity
                 mTitle = getString(R.string.title_section1);
                 break;
             case 2:
-                mTitle = getString(R.string.title_section2);
+                mTitle = getString(R.string.title_section1);
                 break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
+
         }
     }
 
